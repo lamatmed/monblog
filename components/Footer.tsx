4ac4fc67e758261c85b83ea1a0dc3aef929a7825
lamatmed <lamat032025@gmail.com>
2025-01-22 01:09:40 +0100

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='flex items-center justify-center text-sm py-[70px] text-center border-t'>
-      <p>Copyright &copy; - 2025 - La Minute De Code - All rights reserved</p>
+      <p>Copyright &copy; - 2025 - Lamat Abdelahi - All rights reserved</p>
 
     </footer>
   )
